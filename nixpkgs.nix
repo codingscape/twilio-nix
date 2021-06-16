c98ffef15dd9a3c153df4fb8473ff98f0ba700b2
Jordan Mulcahey <jmulcahey@twilio.com>
2021-06-15 19:01:32 -0700

    Bump nixpkgs to 21.05
    Fixes several issues on darwin

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,4 @@
 import
-(fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-20.09.tar.gz") {
+(fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-21.05.tar.gz") {
   overlays = [ (import ./overlay.nix) ];
 }
